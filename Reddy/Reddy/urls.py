6074b596c08dcc44ab2e8ad2199bb5cafb0332c8
reddieabhi <91117253+reddieabhi@users.noreply.github.com>
2023-12-02 11:07:13 +0530

    Your commit message

--- Reddy/Reddy/urls.py
+++ Reddy/Reddy/urls.py
@@ -22,13 +22,32 @@ from django.conf import settings
 from starmaa.views import *
 
 
+
+from django.contrib import admin
+from django.urls import path
+from starmaa.views import *
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",working,name="working"),
-    path("people_view",people_view,name = "people_view"),
-    path("coin_toss",coin_toss,name="coin_toss")
-    
+    path("new_exam/",new_exam, name = 'new_exam'),
+    path('tlg/',Teacher_login_page,name = "Teacher_login_page"),
+    path("view_students/<standard>/",view_students,name = 'view_students'),
+    path('teacher/', Teacher_view, name='Teacher'),
+    path('teacher_second/<st_id>/',teacher_second_page,name = 'teacher_second_page'),
+    #path('logout/',logout_page,name = 'logout_page'),
+    path('teacher/logout/', logout_page, name='logout_page'),
+    path('teacher/logout/tlg/',Teacher_login_page,name = "Teacher_login_page"),
+    path('update_student/<st_id>/<unit_id>/',update_student,name = 'update_student'),
+    path("add_stud/",add_student,name = 'add_student'),
+    path("my_marks/<id1>/<id2>/",view_student, name = 'view_student'),
+    path("stud_login/",stud_login,name = 'stud_login'),
+    path("update_exam/<id1>/",update_exam,name = 'update_exam'),
+    path("delete_exam/<id1>/",delete_exam, name = 'delete_exam'),
+    #path("stud_portal/<id1>/",student_portal , name='student_portal'),
+    path("",home_page,name="home_page")
+
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
